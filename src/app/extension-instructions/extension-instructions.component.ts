233dab1c547b47e83730b88d14afe5cd4bf0bc8c
Miguel Treviño <miguel@verifik.co>
2024-05-09 13:54:46 -0500

    finished instructions UI

--- src/app/extension-instructions/extension-instructions.component.ts
+++ src/app/extension-instructions/extension-instructions.component.ts
@@ -1,8 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
 	selector: "app-extension-instructions",
 	templateUrl: "./extension-instructions.component.html",
 	styleUrls: ["./extension-instructions.component.scss", "../main.scss"],
 })
-export class ExtensionInstructionsComponent {}
+export class ExtensionInstructionsComponent implements OnInit {
+	currentSlide: number = 1;
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	changeToSlide(slide: number): void {
+		this.currentSlide = slide;
+
+		if (this.currentSlide > 2) {
+			//navigate somewhere
+			this.currentSlide = 1;
+		}
+	}
+}

c002686563ee9f8f966ebe83fd50f84a60ff8dc3
alisdair-ashbourne <alisdair.ashbourne@gmail.com>
2025-02-19 14:42:18 -0700

    #14: Updates to the UX of the extension dashboard
    ; Conflicts:
    ;	dist/assets/i18n/en.json
    ;	dist/main.js
    ;	src/app/home/home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 /// <reference types="chrome"/>
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+
 import { BlockchainNetworksService } from "app/blockchain-networks.service";
 import { ChromeService } from "app/chrome.service";
 import { EthereumService } from "app/eth.service";
 import { SolanaService } from "app/solana.service";
 import { Asset, ETHTransaction, Wallet, WalletModel } from "app/wallet";
-import { WalletService } from "app/wallet.service";
 import { Subject, takeUntil } from "rxjs";
 
 @Component({
@@ -15,25 +15,23 @@ import { Subject, takeUntil } from "rxjs";
 	styleUrls: ["./home.component.scss", "../main.scss"],
 })
 export class HomeComponent implements OnInit {
-	private unsubscriber$: Subject<void> = new Subject<void>();
+	private unsubscriber$: Subject<any> = new Subject();
 
-	title: string = "something";
+	activity!: Array<ETHTransaction>;
+	balances: any;
+	balancesLoaded: boolean = false;
+	NFTs!: Array<any>;
+	selectedAsset!: Asset;
+	selectedNetwork!: string;
+	shareables: any;
+	tokens!: Array<any>;
+	view?: string;
 	wallet!: Wallet;
 	wallets!: Array<Wallet>;
-	balances: any;
-	selectedAsset!: Asset;
-	view?: string;
-	shareables: any;
-	activity!: Array<ETHTransaction>;
-	tokens!: Array<any>;
-	NFTs!: Array<any>;
-	selectedNetwork!: string;
-	balancesLoaded: boolean = false;
 
 	constructor(
 		private _router: Router,
 		private route: ActivatedRoute,
-		private _walletService: WalletService,
 		private _ethService: EthereumService,
 		private _chromeService: ChromeService,
 		private _blockchainNetworkService: BlockchainNetworksService,
@@ -44,8 +42,8 @@ export class HomeComponent implements OnInit {
 		this.view = this.route.snapshot.queryParamMap.get("view") || "home";
 
 		this.shareables = {
-			view: this.view,
 			selectedTab: "assets",
+			view: this.view,
 			wallet: {},
 		};
 
@@ -90,7 +88,7 @@ export class HomeComponent implements OnInit {
 		this.unsubscriber$.complete();
 	}
 
-	async _getBalances(): Promise<any> {
+	private async _getBalances(): Promise<any> {
 		if (this.balancesLoaded) return;
 
 		this.tokens = [];
@@ -101,17 +99,70 @@ export class HomeComponent implements OnInit {
 		this.balancesLoaded = true;
 	}
 
-	openFullPage(): void {
-		try {
-			const url = chrome.runtime.getURL("index.html");
+	private async _getETHDetails(): Promise<any> {
+		if (!this.wallet?.ethAddress) return;
 
-			chrome.tabs.create({ url });
-		} catch (exception) {
-			alert(exception);
+		const details = await this._ethService.getWalletDetails(this.wallet.ethAddress);
+
+		this.selectedAsset = new Asset({
+			asset: details.data.account.asset,
+			balance: details.data.balance,
+			fiatBalance: Number(details.data.fiatBalance),
+			price: details.data.account.price,
+		});
+
+		this.activity = [];
+
+		for (let index = 0; index < details.data.transactions.length; index++) {
+			const transaction = details.data.transactions[index];
+
+			this.activity.push(new ETHTransaction(transaction));
 		}
+
+		this._getTokens("Ethereum", details.data.tokenHoldings.tokens);
 	}
 
-	async _setWallet(): Promise<any> {
+	private async _getSolanaDetails(): Promise<any> {
+		const details = await this._solanaService.getWalletDetails(this.wallet.solanaAddress);
+
+		if (!details) return;
+
+		if (details.data.balance) {
+			this.selectedAsset.fiatBalance += Number(details.data.fiatBalance);
+		}
+
+		this._getTokens("Solana", details.data.tokenHoldings.tokens);
+	}
+
+	private _getTokens(network: string, tokens: Array<any>): void {
+		const _tempTokens = [...this.tokens];
+		const _tempNFTs = [...this.NFTs];
+
+		for (let index = 0; index < tokens.length; index++) {
+			const token = tokens[index];
+
+			if (["ERC-20", "ETH"].includes(token.tokenType) && token.price) {
+				_tempTokens.push({ ...token, network });
+			} else if (["NFT"].includes(token.tokenType)) {
+				_tempNFTs.push({ ...token, network });
+			}
+
+			if (network === "Solana") {
+				const _token = { ...token, symbol: token.symbol || token.name, network };
+
+				if (_token.name === "Zelf") {
+					_token.symbol = "ZNS";
+				}
+
+				_tempTokens.push(_token);
+			}
+		}
+
+		this.tokens = _tempTokens;
+		this.NFTs = _tempNFTs;
+	}
+
+	private async _setWallet(): Promise<any> {
 		let wallet = await this._chromeService.getItem("wallet");
 
 		if (!wallet?.ethAddress && !wallet?.solanaAddress) {
@@ -131,81 +182,6 @@ export class HomeComponent implements OnInit {
 		this.shareables.wallet = new WalletModel(wallet);
 
 		return this.shareables.wallet;
-	}
-
-	async _getSolanaDetails(): Promise<any> {
-		if (!this.wallet?.solanaAddress) return;
-
-		const details = await this._solanaService.getWalletDetails(this.wallet?.solanaAddress);
-
-		if (!details) return;
-
-		if (details.data.balance) {
-			this.selectedAsset.fiatBalance += Number(details.data.fiatBalance);
-		}
-
-		this.getTokens("Solana", details.data.tokenHoldings.tokens);
-	}
-
-	async _getETHDetails(): Promise<any> {
-		if (!this.wallet?.ethAddress) return;
-
-		const details = await this._ethService.getWalletDetails(this.wallet.ethAddress);
-
-		this.selectedAsset = new Asset({
-			asset: details.data.account.asset,
-			fiatBalance: Number(details.data.fiatBalance),
-			balance: details.data.balance,
-			price: details.data.account.price,
-		});
-
-		this.activity = [];
-
-		for (let index = 0; index < details.data.transactions.length; index++) {
-			const transaction = details.data.transactions[index];
-
-			this.activity.push(new ETHTransaction(transaction));
-		}
-
-		this.getTokens("Ethereum", details.data.tokenHoldings.tokens);
-	}
-
-	getTokens(network: string, tokens: Array<any>): void {
-		for (let index = 0; index < tokens.length; index++) {
-			const token = tokens[index];
-
-			if (["ERC-20", "ETH"].includes(token.tokenType) && token.price) {
-				this.tokens.push({ ...token, network });
-			} else if (["NFT"].includes(token.tokenType)) {
-				this.NFTs.push({ ...token, network });
-			}
-
-			if (network === "Solana") {
-				const _token = { ...token, symbol: token.symbol || token.name, network };
-
-				if (_token.name === "Zelf") {
-					_token.symbol = "ZNS";
-				}
-
-				this.tokens.push(_token);
-			}
-		}
-	}
-
-	_redirectToOnboarding(): void {
-		this._router.navigate(["/onboarding"]);
-	}
-
-	getAddress(): string {
-		const address = this.wallet?.publicData?.ethAddress || "";
-
-		if (!address) return "";
-
-		return this._walletService.getDisplayableAddress(address);
-	}
-
-	openAccountsPage(): void {
-		this.shareables.view = this.shareables.view === "home" ? "accountsPage" : "home";
 	}
 
 	selectTab(tab: string): void {

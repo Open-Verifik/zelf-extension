c00f0c40b3d7378734c5318cc1361e96ce06683c
migueltrevinom <miguel@verifik.co>
2025-02-28 16:55:05 -0500

    compile fixes of prices

--- src/app/pipes/discount.pipe.ts
+++ src/app/pipes/discount.pipe.ts
@@ -20,6 +20,7 @@ export class DiscountPipe implements PipeTransform {
 		if (discountType === "percentage") {
 			discountedResult = value - (value * discountValue) / 100;
 		} else if (discountType === "amount") {
+			console.log({ value, discountValue });
 			discountedResult = value - discountValue;
 		}
 

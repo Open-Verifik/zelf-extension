c965d1071cb6b957f47170c12239ef0c8a008307
alisdair-ashbourne <alisdair.ashbourne@gmail.com>
2025-03-07 16:43:06 -0700

    #20 Add name search screens

---
+++ src/app/welcome-find/welcome-find.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "welcome-find",
+    standalone: true,
+    imports: [],
+    templateUrl: "./welcome-find.component.html",
+    styleUrls: ["./welcome-find.component.scss"],
+})
+export class WelcomeFindComponent {}

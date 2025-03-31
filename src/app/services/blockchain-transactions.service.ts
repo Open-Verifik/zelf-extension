b0197c81493dd0e7410cb34d3151fa1ae5b03cfc
Dalejo505 <dg4m3s@gmail.com>
2025-03-31 10:43:32 -0500

    Getting tokens in home

--- src/app/services/blockchain-transactions.service.ts
+++ src/app/services/blockchain-transactions.service.ts
@@ -14,42 +14,53 @@ import { Transaction, WalletModel } from "app/wallet";
 export class BlockchainTransactionsService {
     constructor(private _httpWrapperService: HttpWrapperService) {}
 
-    getAddressData(wallet: Partial<WalletModel> | null): Observable<Transaction[]> {
+    getAddressData(wallet: Partial<WalletModel> | null): Observable<any> {
         if (!wallet) return of([]);
 
         return forkJoin({
-            ethereum: this._httpWrapperService.sendRequest("get", `${environment.apiUrl}/api/ethereum/address`, { address: wallet.ethAddress }),
+            ethereum: this._httpWrapperService.sendRequest("get", `${environment.apiUrl}/api/ethereum/address`, {
+                address: wallet.ethAddress,
+            }),
             avalanche: this._httpWrapperService.sendRequest("get", `${environment.apiUrl}/api/avalanche/address/${wallet.ethAddress}`, {}),
             solana: wallet.solanaAddress
                 ? this._httpWrapperService.sendRequest("get", `${environment.apiUrl}/api/solana/address/${wallet.solanaAddress}`, {})
                 : of(null),
         }).pipe(
             map((responses) => {
-                const transactions: Transaction[] = [];
-
-                if (responses.ethereum?.data) {
-                    const ethImage = responses.ethereum.data.tokenHoldings?.tokens?.find((t: any) => t.symbol === "ETH")?.image;
-
-                    if (responses.ethereum.data.transactions) {
-                        transactions.push(
-                            ...responses.ethereum.data.transactions.map((tx: any) => ({
-                                ...tx,
-                                image: ethImage,
-                            }))
-                        );
-                    }
-                }
-
-                if (responses.avalanche?.data?.transactions) {
-                    transactions.push(...responses.avalanche.data.transactions);
-                }
-
-                if (responses.solana?.data?.transactions) {
-                    transactions.push(...responses.solana.data.transactions);
-                }
-
-                return transactions;
+                return {
+                    transactions: this._processTransactions(responses),
+                    ethereum: responses.ethereum,
+                    avalanche: responses.avalanche,
+                    solana: responses.solana,
+                };
             })
         );
+    }
+
+    private _processTransactions(responses: any): Transaction[] {
+        const transactions: Transaction[] = [];
+
+        if (responses.ethereum?.data) {
+            const ethImage = responses.ethereum.data.tokenHoldings?.tokens?.find((t: any) => t.symbol === "ETH")?.image;
+
+            if (responses.ethereum.data.transactions) {
+                transactions.push(
+                    ...responses.ethereum.data.transactions.map((tx: any) => ({
+                        ...tx,
+                        image: ethImage,
+                    }))
+                );
+            }
+        }
+
+        if (responses.avalanche?.data?.transactions) {
+            transactions.push(...responses.avalanche.data.transactions);
+        }
+
+        if (responses.solana?.data?.transactions) {
+            transactions.push(...responses.solana.data.transactions);
+        }
+
+        return transactions;
     }
 }

a943d7f3dd3488c3e880e5d158fa1af63f5d0883
Dalejo505 <dg4m3s@gmail.com>
2025-04-01 15:17:12 -0500

    Add sui functions

--- src/app/blockchain-networks.service.ts
+++ src/app/blockchain-networks.service.ts
@@ -2,32 +2,41 @@ import { Injectable } from "@angular/core";
 import { ChromeService } from "./chrome.service";
 
 @Injectable({
-	providedIn: "root",
+    providedIn: "root",
 })
 export class BlockchainNetworksService {
-	selectedNetwork!: string;
+    selectedNetwork!: string;
+    private currentSuiNetwork: string = "mainnet"; // Default network, adjust as needed
 
-	constructor(private _chromeService: ChromeService) {}
+    constructor(private _chromeService: ChromeService) {}
 
-	async _initNetwork(): Promise<any> {
-		this.selectedNetwork = await this._chromeService.getItem("network");
+    async _initNetwork(): Promise<any> {
+        this.selectedNetwork = await this._chromeService.getItem("network");
 
-		if (!this.selectedNetwork) {
-			this.selectedNetwork = "eth";
+        if (!this.selectedNetwork) {
+            this.selectedNetwork = "eth";
 
-			await this._chromeService.setItem("network", this.selectedNetwork);
-		}
+            await this._chromeService.setItem("network", this.selectedNetwork);
+        }
 
-		return this.selectedNetwork;
-	}
+        return this.selectedNetwork;
+    }
 
-	getSelectedNetwork(): string {
-		return this.selectedNetwork || "eth";
-	}
+    getSelectedNetwork(): string {
+        return this.selectedNetwork || "eth";
+    }
 
-	async setSelectedNetwork(code: string): Promise<any> {
-		this.selectedNetwork = code;
+    async setSelectedNetwork(code: string): Promise<any> {
+        this.selectedNetwork = code;
 
-		await this._chromeService.setItem("network", this.selectedNetwork);
-	}
+        await this._chromeService.setItem("network", this.selectedNetwork);
+    }
+
+    getCurrentSuiNetwork(): string {
+        return this.currentSuiNetwork;
+    }
+
+    setCurrentSuiNetwork(network: string): void {
+        this.currentSuiNetwork = network;
+    }
 }

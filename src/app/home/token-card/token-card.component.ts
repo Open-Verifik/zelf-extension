a54bd67035c71d8862e13918f6afd6ccf98e8f43
alisdair-ashbourne <alisdair.ashbourne@gmail.com>
2025-04-11 10:04:58 -0600

    Update token price display in send and home - gray on the bottom

--- src/app/home/token-card/token-card.component.ts
+++ src/app/home/token-card/token-card.component.ts
@@ -18,10 +18,6 @@ import { Component, Input, OnInit } from "@angular/core";
             </div>
 
             <div class="amount-container" fxLayout="column" fxLayoutAlign="end end">
-                <div class="token-card__balance">
-                    {{ data.fiatBalance | currency : "USD" : "symbol" : "1.2-5" }}
-                </div>
-
                 <h4 class="stats stats--no-margin">
                     <div
                         class="stats__percentage"
@@ -30,23 +26,13 @@ import { Component, Input, OnInit } from "@angular/core";
                             'stats__percentage--negative': false
                         }"
                     >
-                        <!-- <span class="stats__arrow">
-                            <ng-container *ngIf="true">
-                                <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4.5 0.530273L0.5 4.53027L1.44 5.47027L4.5 2.41694L7.56 5.47027L8.5 4.53027L4.5 0.530273Z" />
-                                </svg>
-                            </ng-container>
-
-                            <ng-container *ngIf="false">
-                                <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4.5 5.46973L8.5 1.46973L7.56 0.529726L4.5 3.58306L1.44 0.529726L0.5 1.46973L4.5 5.46973Z" />
-                                </svg>
-                            </ng-container>
-                        </span> -->
-
                         <span class="stats__text stats__text--colored">{{ data.amount }}</span>
                     </div>
                 </h4>
+
+                <div class="token-card__balance">
+                    {{ data.fiatBalance | currency : "USD" : "symbol" : "1.2-5" }}
+                </div>
             </div>
         </div>
     `,

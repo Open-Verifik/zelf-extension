e4584351a38e762935bb3d43d3ab4d519aa30799
alisdair-ashbourne <alisdair.ashbourne@gmail.com>
2025-04-26 11:56:18 -0600

    #87 WIP: adding transloco keys manager

--- src/app/core/transloco-root.module.ts
+++ src/app/core/transloco-root.module.ts
@@ -1,77 +1,13 @@
-import { HttpClient } from "@angular/common/http";
-import { Translation, TranslocoLoader, TranslocoModule, TranslocoService, provideTransloco } from "@jsverse/transloco";
-import { Injectable, NgModule, inject, provideAppInitializer } from "@angular/core";
-import { environment } from "../../environments/environment";
+import { TranslocoModule, TranslocoService } from "@jsverse/transloco";
+import { NgModule, inject, provideAppInitializer } from "@angular/core";
 import { MobileRestrictedComponent } from "./mobile-restricted/mobile-restricted.component";
-
-@Injectable({ providedIn: "root" })
-export class TranslocoHttpLoader implements TranslocoLoader {
-    private http = inject(HttpClient);
-
-    getTranslation(lang: string) {
-        return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
-    }
-}
+import { translocoProvider } from "../../../transloco.config";
 
 @NgModule({
     imports: [TranslocoModule],
     exports: [TranslocoModule],
     providers: [
-        provideTransloco({
-            loader: TranslocoHttpLoader,
-            config: {
-                defaultLang: "en",
-                fallbackLang: "en",
-                reRenderOnLangChange: true,
-                prodMode: environment.production,
-                availableLangs: [
-                    {
-                        id: "en",
-                        label: "English",
-                    },
-                    {
-                        id: "es",
-                        label: "Spanish",
-                    },
-                    {
-                        id: "br",
-                        label: "Portuguese",
-                    },
-                    {
-                        id: "fr",
-                        label: "French",
-                    },
-                    {
-                        id: "ru",
-                        label: "Russian",
-                    },
-                    {
-                        id: "kr",
-                        label: "Korean",
-                    },
-                    {
-                        id: "in",
-                        label: "Hindi",
-                    },
-                    {
-                        id: "cn",
-                        label: "Chinese",
-                    },
-                    {
-                        id: "ph",
-                        label: "Filipino",
-                    },
-                    {
-                        id: "ja",
-                        label: "Japanese",
-                    },
-                    {
-                        id: "ar",
-                        label: "Arabic",
-                    },
-                ],
-            },
-        }),
+        translocoProvider,
         provideAppInitializer(() => {
             const initializerFn = (() => {
                 const translocoService = inject(TranslocoService);
